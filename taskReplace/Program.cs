975eaf08e4523c1d87b36d2c889bbdae1ada34d1
Daria <dashazoueva@gmail.com>
2022-05-18 16:56:53 +0300

    2

--- taskReplace/Program.cs
+++ taskReplace/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дан текст. В тпексте заменить все пробелы на |, маленькие буквы "д" заменить на большие "Д".
+
+string text = "Я думаю, что да.";
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for(int i = 0; i<length; i++)
+    {
+        if(text[i] == oldValue)
+        result = result + $"{newValue}";
+        else
+        result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ','|');
+Console.WriteLine(newText);
